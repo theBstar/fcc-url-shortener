9468626653130d4616416e48b2ffd36365b06b44
Glitch (url-shrotener-fcc) <none>
2018-08-25 04:19:29 +0000

    💺🔦 Checkpoint ./server.js:1639572/407

--- server.js
+++ server.js
@@ -43,10 +43,12 @@ app.post("/api/shorturl/new", function (req, res) {
   dns.lookup(newUrl, function(err){
     if(err){
       console.log("it is not a valid url")
-      toBeSent({"error":"invalid URL"});
+      res.json({"error":"invalid URL"});
     }else{
+      console.log("its is a valid url")
       urlModel.find({originalUrl: newUrl}, function(err, data){
         if(!err){
+          console.log("it already exits in the db "+ data)
           toBeSent = Object.assign({original_url: data.originalUrl, short_url: data.newUrl})
         }else{
           const urlDoc = new urlModel({originalUrl: newUrl, newUrl: Math.floor(Math.random()*1000)})
@@ -54,6 +56,7 @@ app.post("/api/shorturl/new", function (req, res) {
             if(err){
               console.log("error occured")
             }else{
+              console.log("created new doc in db "+ data)
               toBeSent = Object.assign({original_url: data.originalUrl, short_url: data.newUrl})
             }
           })
